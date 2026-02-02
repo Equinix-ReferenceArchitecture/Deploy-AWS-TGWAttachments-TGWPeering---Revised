41c547947fb57da6352952cc4068a7c793096fba
vipin2060 <vipinvikraman86@gmail.com>
2026-02-02 18:36:54 +0800

    Add Terraform provider configurations for AWS and Equinix
    Define required providers for Terraform and configure AWS providers.

---
+++ providers.tf
@@ -0,0 +1,29 @@
+
+terraform {
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = "2.4.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.63.1"
+
+    }  
+  }
+}
+
+
+provider "aws" {
+  region = var.primary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+provider "aws" {
+  alias = "secondary"
+  region     = var.secondary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+} 
+
