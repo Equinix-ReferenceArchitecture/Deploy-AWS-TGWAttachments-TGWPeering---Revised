af47d2bfeb97707904f9bf5a045c23634f03ca07
vipin2060 <vipinvikraman86@gmail.com>
2026-02-02 18:37:18 +0800

    Add variables for AWS and Terraform Cloud
    Define multiple variables for AWS and Terraform Cloud configurations.

---
+++ variables.tf
@@ -0,0 +1,75 @@
+
+variable "authentication_key" {
+  description = "AWS ID"
+  type        = string
+}
+
+variable "aws_access_key" {
+  type    = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = ""
+}
+
+variable "prefixes_from_onprem" {
+  description = "Prefixes allowed from on-prem"
+  type        = list
+}
+
+variable "Terraformcloud_org_name" {
+  description = "This is the org name for terraform cloud"
+  type        = string
+}
+
+variable workspaceforDualDGW-Parent {
+  description = "This is the workspace name of terraform cloud"
+  type        = string
+}
+
+variable "workspaceforDualTGW-Parent" {
+  description = "This is the workspace name of terraform cloud"
+  type        = string
+}
+
+variable "workspaceforDualVPC-Parent" {
+  description = "This is the workspace name of terraform cloud"
+  type        = string
+}
+
+variable "attachment_name1" {
+  description = "This is the TGW attachment name"
+  type        = string
+}
+variable "attachment_name2" {
+  description = "This is the TGW attachment name"
+  type        = string
+}
+
+variable "region_2" {
+  description = "This is the AWS 2nd region"
+  type        = string
+}
+
+variable "route_1_VPC01_to_TGW01" {
+  description = "This is the route added between VPC01 and TGW01"
+  type = string
+}
+
+variable "route_2_VPC02_to_TGW02" {
+  description = "This is the route added between VPC01 and TGW01"
+  type = string
+}
+
+variable "primary_aws_region" {
+  description = "This is the aws primary region"
+  type = string
+}
+
+variable "secondary_aws_region" {
+  description = "This is the aws primary region"
+  type = string
+}
+
